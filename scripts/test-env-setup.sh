2706b956d13809bbc902447d8882efecd5cd9043
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2018-03-13 11:48:26 -0400

    working scripts for setting up and tearing down sentry test env

--- scripts/test-env-setup.sh
+++ scripts/test-env-setup.sh
@@ -1,21 +1,52 @@
-#!/bin/bash
-
+# #!/bin/bash
+# 
 # Creates a test stack for sentry:
 # creates a new redis db,
 # clones the prod sentry db,
 # and configures the sentry test app to use them
-SENTRY_APP_HANDLE=sentry-test
 
-## spin down sentry so we can make config changes
-# TODO uncommone when done dev
-# ./scale-sentry-services 0
+sentry_app_handle=sentry-test
 
-# TODO uncommone when done dev
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+$PROJECT_ROOT/scripts/test-env-scale-sentry-services.sh 0
+
+# Create postgres db and get connection info
 aptible db:clone sentry-production sentry-production-sandbox
 sentry_postgres_url=`aptible db:url sentry-production-sandbox`
 
+SENTRY_DB_NAME=$(echo $sentry_postgres_url | grep -o "[[:alnum:]]*$")
+SENTRY_DB_PASSWORD=$(echo $sentry_postgres_url | awk -F"(:|@)" '{print $3}')
+SENTRY_DB_USER=$(echo $sentry_postgres_url | awk -F":" '{print $2}' | awk -F"/" '{print $NF}')
+SENTRY_POSTGRES_HOST=$(echo $sentry_postgres_url | awk -F"(:|@)" '{print $4}')
+SENTRY_POSTGRES_PORT=$(echo $sentry_postgres_url | awk -F"(:|/)" '{print $(NF-1)}')
 
-
+# Create redis db and get connection info
 aptible db:create --type redis --version 3.0 --environment reify-production sentry-redis-production-sandbox
 sentry_redis_url=`aptible db:url sentry-redis-production-sandbox`
 
+SENTRY_REDIS_HOST=$(echo $sentry_redis_url | awk -F"(:|@)" '{print $(NF-1)}')
+SENTRY_REDIS_PASSWORD=$(echo $sentry_redis_url | awk -F"(:|@)" '{print $3}')
+SENTRY_REDIS_PORT=$(echo $sentry_redis_url | awk -F":" '{print $NF}')
+
+echo "Setting sentry config:
+SENTRY_DB_NAME:        $SENTRY_DB_NAME
+SENTRY_DB_PASSWORD:    $SENTRY_DB_PASSWORD
+SENTRY_DB_USER:        $SENTRY_DB_USER
+SENTRY_POSTGRES_HOST:  $SENTRY_POSTGRES_HOST
+SENTRY_POSTGRES_PORT:  $SENTRY_POSTGRES_PORT
+SENTRY_REDIS_HOST:     $SENTRY_REDIS_HOST
+SENTRY_REDIS_PASSWORD: $SENTRY_REDIS_PASSWORD
+SENTRY_REDIS_PORT:     $SENTRY_REDIS_PORT"
+
+aptible config:set --app $sentry_app_handle \
+        SENTRY_DB_NAME=$SENTRY_DB_NAME \
+        SENTRY_DB_PASSWORD=$SENTRY_DB_PASSWORD \
+        SENTRY_DB_USER=$SENTRY_DB_USER \
+        SENTRY_POSTGRES_HOST=$SENTRY_POSTGRES_HOST \
+        SENTRY_POSTGRES_PORT=$SENTRY_POSTGRES_PORT \
+        SENTRY_REDIS_HOST=$SENTRY_REDIS_HOST \
+        SENTRY_REDIS_PASSWORD=$SENTRY_REDIS_PASSWORD \
+        SENTRY_REDIS_PORT=$SENTRY_REDIS_PORT
+
+## spin up sentry
+$PROJECT_ROOT/scripts/test-env-scale-sentry-services.sh 1
--- scripts/test-env-teardown.sh
+++ scripts/test-env-teardown.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Dispose of dbs and spin down sentry-test app
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-SENTRY_APP_HANDLE=sentry-test
-
-## spin down sentry
-# TODO uncommone when done dev
-#./scale-sentry-services 0
-
+$PROJECT_ROOT/scripts/test-env-scale-sentry-services.sh 0
 aptible db:deprovision --environment reify-production sentry-production-sandbox
 aptible db:deprovision --environment reify-production sentry-redis-production-sandbox
